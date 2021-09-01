082b40e7f2ffa41786dfad2f60e7bd6260e68ccc
trosie3 <79772644+trosie3@users.noreply.github.com>
2021-09-01 09:21:58 -0700

    scraping defs

--- scraping.py
+++ scraping.py
@@ -3,11 +3,30 @@ from splinter import Browser
 from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+import datetime as dt
 
-# set up splinter
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
+def scrape_all():   
+    # set up splinter
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
 
+    # set our news title and paragraph variables
+    news_title, news_paragraph = mars_news(browser)
+
+    # Run all scraping functions and store results in dictionary
+    data = {
+      "news_title": news_title,
+      "news_paragraph": news_paragraph,
+      "featured_image": featured_image(browser),
+      "facts": mars_facts(),
+      "last_modified": dt.datetime.now()
+    }
+
+    # Stop webdriver and return data
+    browser.quit()
+    return data
+
+# ## scrape mars news
 def mars_news(browser):
     # Visit the mars nasa news site
     url = 'https://redplanetscience.com'
@@ -32,7 +51,7 @@ def mars_news(browser):
 
     return news_title, news_p
 
-# ## Featured Images JPL
+# ## Featured Images JPL scrape
 def featured_image(browser):
     # Visit URL
     url = 'https://spaceimages-mars.com'
@@ -58,7 +77,7 @@ def featured_image(browser):
     return img_url
 
 
-# ## Mars facts
+# ## Mars facts scrape
 def mars_facts():
     # Add try/except for error handling
     try:
@@ -72,9 +91,11 @@ def mars_facts():
     df.set_index('Description', inplace=True)
 
     # Convert dataframe into HTML format, add bootstrap
-    return df.to_html()
+    return df.to_html(classes="table table-striped")
 
-browser.quit() 
-
+# block of code tells Flask that our script is complete and ready for action
+if __name__ == "__main__":
+    # If running as script, print scraped data
+    print(scrape_all())
 
 
